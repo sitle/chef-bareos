0538bea50a1acf63a0a270a38a21e0ff87710b86
Ian Smith <ian.smith@pnnl.gov>
2015-06-24 13:46:29 -0700

    tring this, should work with newer version of serverspec now

--- test/integration/server/serverspec/spec_helper.rb
+++ test/integration/server/serverspec/spec_helper.rb
@@ -2,9 +2,11 @@ require 'serverspec'
 
 set :backend, :exec
 
+
 RSpec.configure do |c|
   c.before :all do
-    c.path = '/sbin:/usr/sbin:/usr/bin'
+   # c.path = '/sbin:/usr/sbin:/usr/bin'
+    set :path, '$PATH:/sbin:/usr/local/sbin'
   end
 
   if ENV['ASK_SUDO_PASSWORD']
