16758c7d203dd238bc9ded86718e6e120a3b1aee
Ian Smith <ian.smith@pnnl.gov>
2015-06-26 14:28:02 -0700

    updating openssl lwrp password generation syntax, was using depricated code

--- recipes/server.rb
+++ recipes/server.rb
@@ -17,8 +17,8 @@
 # limitations under the License.
 #
 
-node.set_unless['bareos']['dir_password'] = secure_password
-node.set_unless['bareos']['mon_password'] = secure_password
+node.set_unless['bareos']['dir_password'] = random_password(length: 30, mode: :base64) 
+node.set_unless['bareos']['mon_password'] = random_password(length: 30, mode: :base64)
 node.save unless Chef::Config[:solo]
 
 # Installation des services BAREOS
