2c8b923d41b111b733dacc1cfb479172b661d4b9
Ian Smith <ian.smith@pnnl.gov>
2015-06-30 16:22:01 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'leonard.tavae@informatique.gov.pf'
 license 'Apache 2.0'
 description 'Installs/Configures bareos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.1.4'
 
 supports 'centos', '>= 6.0'
 supports 'debian', '>= 7.0'
