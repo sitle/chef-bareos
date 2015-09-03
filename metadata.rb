4c7cb50febbb74267cbfccc6fa2806497f1fd77b
Ian Smith <ian.smith@pnnl.gov>
2015-09-03 11:23:13 -0700

    bumping for testing updates

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'leonard.tavae@informatique.gov.pf'
 license 'Apache 2.0'
 description 'Installs/Configures bareos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.1.4'
 
-supports 'centos', '>= 6.0'
 supports 'debian', '>= 7.0'
 supports 'ubuntu', '>= 12.04'
+supports 'centos', '>= 6.0'
 
 depends 'apt'
 depends 'openssl'
