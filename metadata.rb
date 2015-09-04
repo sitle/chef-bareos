ae2e3b03d9bc137d8a6b4c976904931661ad1e47
Ian Smith <ian.smith@pnnl.gov>
2015-09-04 16:14:57 -0700

    limiting this cookbook to debian 7 or so as we haven't tested directly on debain 8 yet

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures bareos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.4'
 
-supports 'debian', '>= 7.0'
+supports 'debian', '~> 7.0'
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 6.0'
 
