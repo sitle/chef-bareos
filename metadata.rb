05c2e11328e940096ed369426ca7875f28cef6ae
Ian Smith <ian.smith@pnnl.gov>
2015-09-23 11:51:21 -0700

    major version bump, users caution, much refactoring, apologies on that, adding fedora support

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'leonard.tavae@informatique.gov.pf'
 license 'Apache 2.0'
 description 'Installs/Configures bareos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '1.0.1'
 
-supports 'debian', '~> 7.0'
-supports 'ubuntu', '>= 12.04'
-supports 'centos', '>= 6.0'
+%w( centos fedora debian ubuntu ).each do |os|
+  supports os
+end
 
 depends 'apt'
 depends 'openssl'
