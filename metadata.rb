23ddf64c3c230f92d006d7ee503f641a9753dfd3
Ian Smith <ian.smith@pnnl.gov>
2015-09-24 00:57:35 -0700

    reworking where defining variables

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'leonard.tavae@informatique.gov.pf'
 license 'Apache 2.0'
 description 'Installs/Configures bareos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.2'
+version '1.0.3'
 
 %w( centos fedora debian ubuntu ).each do |os|
   supports os
