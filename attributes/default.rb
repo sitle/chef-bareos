41d38ff8c96a94f183c390ac030cae52d8d5af35
Ian Smith <ian.smith@pnnl.gov>
2015-10-02 17:03:55 -0700

    Missing the .0 at end of platform url attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,8 +15,8 @@ case node['platform_family']
 when 'debian'
   case node['platform']
   when 'debian'
-    default['bareos']['baseurl'] = "#{node['bareos']['url']}/#{node['bareos']['version']}/Debian_#{node['platform_version']}/"
-    default['bareos']['gpgkey'] = "#{node['bareos']['url']}/#{node['bareos']['version']}/Debian_#{node['platform_version']}/Release.key"
+    default['bareos']['baseurl'] = "#{node['bareos']['url']}/#{node['bareos']['version']}/Debian_#{node['platform_version'].to_i}.0/"
+    default['bareos']['gpgkey'] = "#{node['bareos']['url']}/#{node['bareos']['version']}/Debian_#{node['platform_version'].to_i}.0/Release.key"
   when 'ubuntu'
     default['bareos']['baseurl'] = "#{node['bareos']['url']}/#{node['bareos']['version']}/xUbuntu_#{node['platform_version']}/"
     default['bareos']['gpgkey'] = "#{node['bareos']['url']}/#{node['bareos']['version']}/xUbuntu_#{node['platform_version']}/Release.key"
