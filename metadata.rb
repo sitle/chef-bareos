e659c9837f20b6c48e852fce84076e9ceeefb170
Ian Smith <ian.smith@pnnl.gov>
2016-01-12 12:26:12 -0800

    adding in new attribute to split the client local name versus network name possible differences

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'leonard.tavae@informatique.gov.pf'
 license 'Apache 2.0'
 description 'Installs/Configures bareos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.7'
+version '2.1.8'
 
 %w( centos redhat ).each do |os|
   supports os, '>= 6.0'
