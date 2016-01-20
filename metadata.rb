9e19152dcfe201c0d13d6947d49c7b63837bd794
Ian Smith <ian.smith@pnnl.gov>
2016-01-19 19:16:25 -0800

    Removing the postgresql cookbook constraint of '< 4.0.0'

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'leonard.tavae@informatique.gov.pf'
 license 'Apache 2.0'
 description 'Installs/Configures bareos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.24'
+version '2.2.1'
 
 %w( centos redhat ).each do |os|
   supports os, '>= 6.0'
@@ -16,5 +16,5 @@ end
 
 depends 'apt'
 depends 'openssl'
-depends 'postgresql', '< 4.0.0'
+depends 'postgresql'
 depends 'yum'
