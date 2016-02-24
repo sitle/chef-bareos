df6e5299b0002a47d538e7cb0072163a2f8cefd2
I Smith <ian.smith@pnnl.gov>
2016-02-24 13:20:42 -0800

    This is starting to work for me a little better, getting some os seperation as well

--- spec/unit/recipes/repo_spec.rb
+++ spec/unit/recipes/repo_spec.rb
@@ -3,27 +3,45 @@
 require 'spec_helper'
 
 describe 'chef-bareos::repo' do
-  context 'Source is needed,' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
+
+
+  ['6.7', '7.2.1511'].each do |version|
+    context "on CentOS #{version}" do
+      before do
+        Fauxhai.mock(platform: 'CentOS', version: version)
+      end
+
+      let(:chef_run) do
+        runner = ChefSpec::ServerRunner.new
+        runner.converge('chef-bareos::repo')
+      end
+
+      it 'converges successfully' do
+        expect { chef_run }.to_not raise_error
+      end
+
+      it 'should install bareos repo'
+      it 'should install bareos contrib repo'
     end
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-    it 'installing repo...'
   end
 
-  context 'Packages required,' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-    it 'updating yum cache for main bareos packages...' do
-      expect { chef_run }.to yum_repository(default['bareos']['yum_repository'])
+  ['12.04', '14.04'].each do |version|
+    context "on Ubuntu #{version}" do
+      before do
+        Fauxhai.mock(platform: 'ubuntu', version: version)
+      end
+
+      let(:chef_run) do
+        runner = ChefSpec::ServerRunner.new
+        runner.converge('chef-bareos::repo')
+      end
+
+      it 'converges successfully' do
+        expect { chef_run }.to_not raise_error
+      end
+
+      it 'should install bareos repo'
+      it 'should install bareos contrib repo'
     end
   end
 end
