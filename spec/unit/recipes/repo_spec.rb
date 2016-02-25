d0d25854db407ab77d707f617874e0234e5082c3
I Smith <ian.smith@pnnl.gov>
2016-02-24 16:30:42 -0800

    need to plan these tests out a bit more, skeleton in place, needs work..

--- spec/unit/recipes/repo_spec.rb
+++ spec/unit/recipes/repo_spec.rb
@@ -3,25 +3,28 @@
 require 'spec_helper'
 
 describe 'chef-bareos::repo' do
-
-
   ['6.7', '7.2.1511'].each do |version|
     context "on CentOS #{version}" do
       before do
-        Fauxhai.mock(platform: 'CentOS', version: version)
+        Fauxhai.mock(platform: 'centos', version: version)
       end
 
       let(:chef_run) do
-        runner = ChefSpec::ServerRunner.new
-        runner.converge('chef-bareos::repo')
+        ChefSpec::SoloRunner.new do |node|
+        end.converge('chef-bareos::repo')
       end
 
       it 'converges successfully' do
         expect { chef_run }.to_not raise_error
       end
 
-      it 'should install bareos repo'
-      it 'should install bareos contrib repo'
+      it 'should add bareos yum repo' do
+        # expect(chef_run).to add_yum_repository('bareos')
+      end
+
+      it 'should add bareos_contrib yum repo' do
+        # expect(chef_run).to add_yum_repository('bareos_contrib')
+      end
     end
   end
 
@@ -32,16 +35,29 @@ describe 'chef-bareos::repo' do
       end
 
       let(:chef_run) do
-        runner = ChefSpec::ServerRunner.new
-        runner.converge('chef-bareos::repo')
+        ChefSpec::SoloRunner.new do |node|
+        end.converge('chef-bareos::repo')
       end
 
       it 'converges successfully' do
         expect { chef_run }.to_not raise_error
       end
 
-      it 'should install bareos repo'
-      it 'should install bareos contrib repo'
+      it 'should add bareos apt repo' do
+        # expect(chef_run).to add_apt_repository('bareos').with(
+        #   'uri' => node['bareos']['baseurl'],
+        #   'key' => node['bareos']['gpgkey'],
+        #   'components' => ['/']
+        # )
+      end
+
+      it 'should add bareos_contrib apt repo' do
+        # expect(chef_run).to add_apt_repository('bareos_contrib').with(
+        #   'uri' => node['bareos']['contrib_baseurl'],
+        #   'key' => node['bareos']['contrib_gpgkey'],
+        #   'components' => ['/']
+        # )
+      end
     end
   end
 end
