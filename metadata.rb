96b3cf169f59bc97d74ddea970de3f62109f6c5f
I Smith <ian.smith@pnnl.gov>
2016-05-10 13:47:12 -0700

    General Cleanup

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name 'chef-bareos'
-maintainer 'dsi'
-maintainer_email 'leonard.tavae@informatique.gov.pf'
-license 'Apache 2.0'
-description 'Installs/Configures bareos'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name              'chef-bareos'
+maintainer        'dsi'
+maintainer_email  'leonard.tavae@informatique.gov.pf'
+license           'Apache 2.0'
+description       'Installs/Configures BAREOS - Backup Archiving REcovery Open Sourced'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '3.0.1'
 
 %w( centos redhat ).each do |os|
