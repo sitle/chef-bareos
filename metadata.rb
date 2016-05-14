4a6ec9e8c7dae8de1c9de468ac9b0202af1a1027
I Smith <ian.smith@pnnl.gov>
2016-05-13 17:37:45 -0700

    Minor update to version for README updates

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Installs/Configures BAREOS - Backup Archiving REcovery Open S
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url        'https://github.com/sitle/chef-bareos/issues'
 source_url        'https://github.com/sitle/chef-bareos.git'
-version           '3.0.1'
+version           '3.0.2'
 
 %w( centos redhat ).each do |os|
   supports os, '>= 6.0'
