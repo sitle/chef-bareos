a8ebf61ccad586433acbf569a5c4de04ec5e1089
I Smith <ian.smith@pnnl.gov>
2016-10-11 15:18:52 -0700

    updating chefspec supported OS testing

--- spec/support/supported_platforms.rb
+++ spec/support/supported_platforms.rb
@@ -2,6 +2,6 @@ def supported_platforms
   _platforms = {
     'ubuntu' => ['14.04'],
     'redhat' => ['6.6', '7.1'],
-    'centos' => ['6.7', '7.2.1511']
+    'centos' => ['6.8', '7.2.1511']
   }
 end
